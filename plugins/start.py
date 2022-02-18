9ea12eb5886e81360e1590bf4fdb862760f081dc
SLOM2539 <94549888+SLOM2539@users.noreply.github.com>
2022-02-18 18:49:27 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,9 +1,9 @@
 from pyrogram import Client, Filters, StopPropagation
 
 
-@Client.on_message(Filters.command(["start"]), group=-2)
+@Client.on_message(Filters.command(["starttt"]), group=-2)
 async def start(client, message):
     # return
-    welcomed = f"**🧸 ┇  مرحبا بك : <b>{message.from_user.first_name}</b>\n\nيمكنك تحميل من يوتيوب بأستخدام البوت .\nارسل رابط الاغنية فقـط -- -- -- -- -- -- -- -- -- -- -- -- -- --**"
+    welcomed = f"**• مرحبا بك : <b>{message.from_user.first_name}</b>\n\n• يمديك تحميل من يوتيوب بأستخدام البوت .\n• ارسل رابط الاغنية بس -- -- -- -- -- -- -- -- -- -- -- -- -- --**"
     await message.reply_text(welcomed)
     raise StopPropagation
